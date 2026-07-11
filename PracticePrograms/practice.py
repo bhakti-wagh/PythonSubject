c94f0d712217c237b217eb9684ef7e83bdd54f37
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-07-11 10:33:21 +0530

    create practice.py

---
+++ PracticePrograms/practice.py
@@ -0,0 +1,75 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+
+a=''
+a
+''
+a=str()
+a
+''
+b=str()
+b
+''
+
+y=
+SyntaxError: invalid syntax
+
+y="PROGRAMMING"
+
+y[4:8:1]
+'RAMM'
+
+y[4:8:3]
+'RM'
+
+y[4:11:2]
+'RMIG'
+
+
+d="First step to learn new things"
+
+
+d[2:5:1]
+'rst'
+
+d[11:13:1]
+'to'
+
+d[20:23:1]
+'new'
+
+d[27:30:1]
+'ngs'
+
+d[6:10:3]
+'sp'
+>>> 
+>>> d[0:5:4]
+'Ft'
+>>> 
+>>> d[0:5:2]
+'Frt'
+>>> 
+>>> d[26:28:1]
+'in'
+>>> 
+>>> d[20:23:2]
+'nw'
+>>> 
+>>> d[24:29:2]
+'tig'
+>>> 
+>>> 
+>>> 
+>>> 
+>>> x="be active in class"
+>>> 
+>>> x[-10:-17:2]
+''
+>>> x[-10:-17:-2]
+'eic '
+>>> x[-10:-17:-6]
+'e '
+>>> x[-10:-17:-1]
+'evitca '
