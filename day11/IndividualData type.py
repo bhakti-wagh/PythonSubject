603084fc7baa3f2c1b5ebcc559eb8013f2a2e133
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-07-17 10:38:14 +0530

    create individual data type

---
+++ day11/IndividualData type.py
@@ -0,0 +1,67 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+>>> #Integer(int):-> It is a Real number without any decimal point in it.
+>>> #               Integer can be both +ve and -ve value
+>>> 
+>>> # The range of integer is form -infinity to +infinity
+>>> 
+>>> #Every Data type will consisit of 2 values
+>>> #1. Default values
+>>> #2. Non-default values
+>>> 
+>>> 
+>>> #1. Default values:-> It is nothing but starting value or initial value
+>>> #Syntax:-> to check default value :-> datatype()(int()>>0)
+>>> #default values are internally considered as 'False'
+>>> 
+>>> #2. Non-default values:-> other the default value all the other values are knows as non-default values
+>>> 
+>>> a=15
+>>> print(int(a))
+15
+>>> 
+>>> type(a)
+<class 'int'>
+
+
+
+
+#float data type:-> It is a real number with decimal point in it
+#  float values can be both +ve and -ve value
+#the range of float is form -infinity to -infinity
+
+#every data type will consist of 2 values
+#1. Default values
+#2. Non-default values
+
+#1. Default values:-> It is nothing but starting value or initial value
+#Syntax:-> to check default value :-> datatype()(flaot()>>0.0)
+#default values are internally considered as 'False'
+#2. Non-default values:-> other the default value all the other values are knows as non-default values
+
+b=18.45
+type(b)
+<class 'float'>
+
+b=18.
+type(b)
+<class 'float'>
+
+b= .
+SyntaxError: invalid syntax
+
+b= .45
+type(b)
+<class 'float'>
+
+
+
+
+a=10
+type(a)
+<class 'int'>
+
+print(int(a))
+10
+
