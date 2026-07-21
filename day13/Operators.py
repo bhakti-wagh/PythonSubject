25fec111b1ea2a7cafec816f9de4de6b0e7fcbd8
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-07-21 09:01:47 +0530

    create operators.py

---
+++ day13/Operators.py
@@ -0,0 +1,30 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+#Operators:->
+
+>>> #It is a librayr function or a special symbol,used to perform a specific task.
+>>> #All operators task is predefined hence we can only access the functionality of operator    but we can't modify it.
+>>> #To perform any operation we required  operand operator.
+>>> 
+>>> #There are  7 operators in python:->
+>>> #1.Arithematic Operator
+>>> #2.Relational Operator
+>>> #3.logical operator
+>>> #4.assignment operator
+>>> #5.Bitwise Operator#3.logical operator
+>>> #6.Membership
+>>> #7.Identity operator
+>>> 
+>>> 
+>>> 
+>>> 
+>>> #1.Arithematic Operator:->
+>>> 
+>>> 
+>>> #1.Addition(+)
+>>> #2.Substraction(-)
+>>> #3.Multiplication(*)
+>>> #4.Division(/):->  1.True Division(/)
+>>> #                  2. Floor Division(//)
+>>> #                  3. Modules operator(%)
