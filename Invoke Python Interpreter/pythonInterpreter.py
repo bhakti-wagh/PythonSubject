26596f329f0c6d881a8b144b51e9e1c5e0a6fb6e
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-07-23 22:24:28 +0530

    create PythonInterpreter file

---
+++ Invoke Python Interpreter/pythonInterpreter.py
@@ -0,0 +1,15 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+>>> #To Invoke Python Interpreter then:
+>>> 
+>>> # first if you using (Window) then :
+>>> 
+>>> #1.call cmd
+>>> #2.then check version of python :-> python --version OR py --version
+>>> #3.then type :-> python OR python.exe
+>>> #4.then run python program
+>>> 
+>>> 
+>>> # for linux & unix then"
+>>> # pass :-> python3.14
