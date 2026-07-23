6d119daa52f73b545095f204c81683ddd1c8686d
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-07-23 14:01:29 +0530

    create assignmenet operators

---
+++ day15/AssignmentOperators.py
@@ -0,0 +1,25 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+>>> # Assignment Operators:->
+>>> # To assign any value to a variable we required assignment operator.
+>>> #Syntax:-> var=value
+>>> 
+>>> a=10
+>>> a+=10
+>>> a
+20
+>>> 
+>>> a-=10
+>>> a
+10
+>>> 
+>>> 
+>>> a/=2
+>>> a
+5.0
+>>> 
+>>> a*=2
+>>> a
+10.0
+>>> 
