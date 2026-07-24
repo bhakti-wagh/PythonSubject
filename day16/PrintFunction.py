7062ef8301c3540df9e1de52a07f656aac9ae03d
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-07-24 09:53:49 +0530

    update printfunction

--- day16/PrintFunction.py
+++ day16/PrintFunction.py
@@ -1,12 +1,12 @@
 Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
->>> 
->>> #Print statement or Function
->>> #print():->
->>> #           To display output in python [on screen] we use print().
->>> #Syntax:-> print(val1,val2,....valN,end='\n',sep='')-> sep=bydefault space shows
->>> 
->>> #-> end=''-> it will take next line as value
+
+#Print statement or Function
+#print():->
+#           To display output in python [on screen] we use print().
+#Syntax:-> print(val1,val2,....valN,end='\n',sep='')-> sep=bydefault space shows
+
+#-> end=''-> it will take next line as value
 >>> 
 >>> print("hello")
 hello
@@ -26,4 +26,10 @@ hello%Good%Morning
 >>> 
 >>> 
 >>> 
-
+>>> d="Bhakti"
+>>> print(a,b,c,end='d')
+hello Good Morningd
+>>> print(a,b,c,end=(d))
+hello Good MorningBhakti
+>>> 
+>>> 
