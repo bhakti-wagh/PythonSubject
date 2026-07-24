9e5bf27bcf528dcd16180a7322548f67af143802
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-07-24 09:26:38 +0530

    create PrintFunction file

---
+++ day16/PrintFunction.py
@@ -0,0 +1,28 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+>>> #Print statement or Function
+>>> #print():->
+>>> #           To display output in python [on screen] we use print().
+>>> #Syntax:-> print(val1,val2,....valN,end='\n',sep='')-> sep=bydefault space shows
+>>> 
+>>> #-> end=''-> it will take next line as value
+>>> 
+>>> print("hello")
+hello
+>>> print("hello") print("good") print("Morning")
+SyntaxError: invalid syntax
+>>> 
+>>> a="hello"
+>>> b="Good"
+>>> c="Morning"
+>>> 
+>>> print(a,b,c)
+hello Good Morning
+>>> print(a,b,c,end='')
+hello Good Morning
+>>> print(a,b,c,end='',sep='%')
+hello%Good%Morning
+>>> 
+>>> 
+>>> 
