41f775528de17e4e2397824371f6fca1c295a4e1
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-07-27 14:40:19 +0530

    create stringlength file

---
+++ day19/conditionalStatement/Simple if/Programs Simple If/StringLength.py
@@ -0,0 +1,36 @@
+
+#Sting Length:->
+
+#Wap tp check whether the give word has odd length
+
+a="PythonProgramming"
+
+if len(a)%2!=0:
+    print(f"The given word {a} is odd length")
+    print(len(a))
+
+    
+#wap to check whether the length of word is divisible by 3
+
+b="PythonProgramming "
+
+if (len(b)//3)*3==len(b):
+    print(f"The given word {b} is divisible by 3")
+    print(len(a))
+
+
+
+#wap to check whether the length of word is greater than 5
+
+c="Apple"
+
+if len(c)>=5:
+    print(f"The given word {c} len is greater than 5")
+
+
+#wap to check whether the length of word is less than 10
+
+d="AppleSweet"
+
+if len(d)<=10:
+    print(f"The given word {d} is less than 10")
