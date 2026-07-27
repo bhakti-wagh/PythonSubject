5122ad79b0e2c12c639c4ec33430e3d691e34d64
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-07-27 09:44:17 +0530

    updated simpleIf file

--- day19/conditionalStatement/SimpleIf.py
+++ day19/conditionalStatement/SimpleIf.py
@@ -1,2 +1,32 @@
 Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
+
+#Conditional statement:
+# Control the flow of program execution based on the condition
+
+#Types of conditional statement:
+#1. Simple if/ if condition
+#2. if-else condition
+#3. elif condition
+#4. nested if-else condition
+
+
+
+#1.Simple if/ if condition:->
+
+#If the give condition is working it will execute inside the True state Block(TSB)
+#If the give condition is not working / satisfed it eill show blank space
+
+#Flowchart:->
+  # start->condition(True/False)->True(it will execute in TSB)
+                               #->False(it will skip the code it will show blankspace)
+  #OR
+
+#Syntax:->   if condition: ->Expression(True/False) -> code is started-> (:) mandatory/showErr
+            #<->  statement --> True state Block
+            #/(if we miss space it will show Indentation Error)
+
+  #If we miss (:) it will show (HumanError) 
+         
+
+
