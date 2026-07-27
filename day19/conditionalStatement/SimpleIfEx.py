99ffb3b1afddadb14a459072dc01154de949ed0f
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-07-27 09:55:54 +0530

    updated files

---
+++ day19/conditionalStatement/SimpleIfEx.py
@@ -0,0 +1,18 @@
+
+#Write a program to check give number is +ve (by using user-input)
+
+
+'''
+num=eval(input("Enter the number:"))
+
+if num>0:
+    print(f"The give number {num} is +ve")
+    
+'''
+
+#Wap to check give number is -ve (by using user-input)
+
+num=eval(input("Enter the Number :"))
+
+if num<0:
+    print(f"The given number {num} is -ve")
