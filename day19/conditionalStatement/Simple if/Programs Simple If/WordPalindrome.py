bf0b1f3164df155aa93b9cab886b47750a139b38
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-07-28 09:43:51 +0530

    updated files

--- day19/conditionalStatement/Simple if/Programs Simple If/WordPalindrome.py
+++ day19/conditionalStatement/Simple if/Programs Simple If/WordPalindrome.py
@@ -32,7 +32,7 @@ if (p//100)==(p%10):
 
 
 #wap to check the given number is divisible by  2 and 6
-
+'''
 num=eval(input("Enter number:"))
 
 if (num%2)==0 and (num%6)==0: #here if 1 condition is true and 2nd condtion false and it will not work 
@@ -44,3 +44,39 @@ num1 = eval(input("Enter Number:"))
 
 if num1%2==0 or num1%6==0:
     print(f"{num1} is divisible by 2 or 6")
+
+'''
+#wap to check given number is divisible by 10
+
+num3=eval(input("Enter num"))
+'''
+if num3%10==0:
+    print(f"{num3} is divisible by 10")
+
+'''
+#wap to check given number is multiple by 7
+
+if num3%7==0:
+    print(f"{num3} is divisible by 7")
+
+#check the word is present in string
+a="Python" #(use 'in ' operator)
+
+if 'n' in a:
+    print("present")
+
+
+#wap to check the given string last character endswith 'k'
+
+b="good luck"
+
+if b.endswith('k'):
+    print("present at end")
+
+
+#wap to check the given string begining "g"
+
+c="good luck"
+
+if c.startswith('g'):
+    print("Present at begining")
