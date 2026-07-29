deccbbccabea97c98b64f5e4ca0192073de0a565
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-07-29 09:31:59 +0530

    create if else files

---
+++ day19/conditionalStatement/if-else/IFElsenote.py
@@ -0,0 +1,24 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+>>> #if-else statement:
+>>> 
+>>> # if the given condition True it will execute in True state block but
+>>> # if the given condition become False it will won't show blank space it
+>>> # will execute in False State Block(else block)
+>>> 
+>>> #syntax:->>
+>>> #           if condition:
+>>> #           <-> statement ->(TSB)
+>>> #           else:
+>>> #               statement->(FSB)
+>>> 
+>>> 
+>>> 
+>>> #----FlowChart-----
+>>> 
+>>> #           start -> condition -> True -> True State Block
+>>> #                              -> False -> False state Block
+>>> 
+>>> #  or
+>>> 
