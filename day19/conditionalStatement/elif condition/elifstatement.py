09ecb280a0dd9566f4606441413f49d33f15efb4
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-01 09:09:13 +0530

    updated elifstatement file

--- day19/conditionalStatement/elif condition/elifstatement.py
+++ day19/conditionalStatement/elif condition/elifstatement.py
@@ -1,9 +1,42 @@
 Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
->>> 
->>> #Elif Condition:
->>> 
->>> # when we have multiple condition we can use elif block
->>> #in elif condition we can use else block but else block is optional
->>> # in elif condition we can use multiple elif block  based on the condition
 
+#Elif Condition:
+
+# when we have multiple condition we can use elif block
+#in elif condition we can use else block but else block is optional
+# in elif condition we can use multiple elif block  based on the condition
+
+#Syntax:->
+
+#           if condition:
+#           <->   statement
+#            |
+#           elif condition:
+#           <->    Statement
+>>> #            |
+... #           elif condition:
+... #           <->    Statement
+... 
+>>> #           else:
+>>> #               condition  ->optional block
+>>> 
+>>> 
+>>> 
+>>> 
+>>> #---> fLOWCHART<-------
+>>> 
+>>> #           start
+>>> #             |
+>>> #           Condition
+>>> #              |
+>>> #         /[TRUE]  \[FAlSE]
+>>> #           |          |
+>>> #    block execute ] [condition2]
+>>> #                         |
+>>> #                 /[TRUE]     \[FAlSE]
+>>> #           [block execute ]  [condition3]
+>>> #                                   |
+>>> #                           /[TRUE]     \[FAlSE]
+>>> #                               |          |
+... #                    [block execute ]   [BlankSpace]
