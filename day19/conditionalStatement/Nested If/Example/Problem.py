375f17da51a7f1277d3dfca5b45c16d40d647397
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-03 10:12:14 +0530

    create nested if

---
+++ day19/conditionalStatement/Nested If/Example/Problem.py
@@ -0,0 +1,90 @@
+
+#wap to check whether the middle value in a list is str or not
+'''
+a=eval(input("Enter the list:"))
+
+
+if len(a)%2==1:
+    if type(a[len(a)//2])==str:
+        print("The middle is string",a[len(a)//2])
+    else:
+        print("The middle value is not string")
+else:
+    print("The length is even and no middle value")
+
+
+'''
+
+#wap to check whether the character is vowel or not
+
+'''
+char=eval(input("Enter character:"))
+
+if char.isalpha():
+    if char in 'AEIOUaeiou':
+        print("character is vowel")
+    else:
+        print("character is consonant")
+else:
+    print("Character is not string")
+'''
+
+
+#wap to check whether the last value in a list is palindrome or not and start with vowel or not
+'''
+char=eval(input("Enter list data:"))
+
+if char[-1]==char[-1][::-1]:
+    if char[-1][0] in 'AEIOUaeiou':
+        print(char[-1],"last ele is palindrome and starts with vowel")
+    else:
+          print(char[-1],"last ele is palindrome and starts with consonant")
+else:
+    print("last value/element is not a plindrome ")
+    
+'''
+
+#wap to check my instagram username and password
+'''
+u='admin'
+p='admin@123'
+user=(input("Enter username:")) 
+if user==u:
+    passw=(input("Enter Password:"))
+    if passw==p:
+        print("Login successful")
+    else:
+        print("Password invalid")
+else:
+    print("username invalid")
+
+'''
+#Greatest of four number
+
+num=eval(input("Enter number:"))
+a=eval(input("Enter number:"))
+b=eval(input("Enter number:"))
+c=eval(input("Enter number:"))
+
+if num >= a:
+    if num >= b:
+        if num >= c:
+            print(f"{num} is greater")
+        else:
+            print(f"{c} is greater")
+    else:
+        if b >= c:
+            print(f"{b} is greater")
+        else:
+            print(f"{c} is greater")
+else:
+    if a >= b:
+        if a >= c:
+            print(f"{a} is greater")
+        else:
+            print(f"{c} is greater")
+    else:
+        if b >= c:
+            print(f"{b} is greater")
+        else:
+            print(f"{c} is greater")
---
+++ day19/conditionalStatement/Nested If/nestedIf.py
@@ -0,0 +1,19 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Nested If statement:->
+... 
+... #A if statement iside another if statment is known as nested if statment
+... 
+... #synax->
+... #           if condition1:
+... #                   if condition2:
+>>> #                           if condition3:
+>>> #
+>>> 
+>>> #                           else:
+>>> #                               FSB3
+>>> #                   else:
+>>> #                       FSB2
+>>> #           else:
+>>> 
+= RESTART: C:/Users/Bhakti/Documents/GitHub/PythonSubject/day19/conditionalStatement/Nested If/Example/Problem.py
