2786f7e2f58b7aa594f87fec7611d9ce662693e8
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-05 09:29:58 +0530

    updated folder looping

--- day19/Looping Statement/looping/loopstatement.py
+++ day19/Looping Statement/looping/loopstatement.py
@@ -1,7 +1,49 @@
 Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
+
+
+#Looping:->
+
+#Set of instruction it will execute again and agian
+#untill condition become False.
+
+
+#Looping Types [2]:
+
+#1. For loop
+#2.While loop
+
+
+#1.for loop:
+
+# - When we know the number of iteration(repetation)
+# - in forloop increment(+) and decrement(-) it will happen
+#   internally (we can't do manually)
+
+
+>>> #syntax:
+>>> #           for variable in iterable:
+>>> #           <->  statement
+>>> 
+>>> #    for-> keyword-> it will start the code
+>>> #    variable-> in the place of variable we can use any haracter because it will
+>>> #               hold the iterable data
+>>> #   in -> operator
+>>> #   iterable-> all collection data type[string ,list,tuple,set,dictonary]
+>>> #   If we miss : then it will show syntax error
+>>> #   If we miss tab space it will show indentation error
 >>> 
 >>> 
->>> #Looping:->
 >>> 
->>> #Set of instruction it will execute again and agian
+>>> 
+>>> # In For Loop there 8 syntax is present:
+>>> 
+>>> #1.Traversing ()
+>>> #2.range()
+>>> #3.reversed()
+>>> #4.enumerate()
+>>> #5.reversed()
+>>> #5.Zip()
+>>> #6.Zip-longest()
+>>> #7.sorted()
+>>> #8.default dict()
