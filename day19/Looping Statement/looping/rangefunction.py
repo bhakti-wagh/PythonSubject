2a54931531913039f4f481c62a05b381a9f0e0e7
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-06 09:08:53 +0530

    range function added file

---
+++ day19/Looping Statement/looping/rangefunction.py
@@ -0,0 +1,20 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+>>> 
+>>> # Range():->
+>>> 
+>>> # inbuilt function
+>>> 
+>>> # if we want to print sequence of number
+>>> 
+>>> 
+>>> # range ()--> syntax:->  for variable in range(startNo,EndNo,stepvalue):
+>>> #                        <->        statement
+>>> 
+>>> # range()-> always it will accept number, range function it will won't accept
+>>> #           any character
+>>> 
+>>> # startNo-> bydefault-0
+>>> # endno-> bydefault- endno+1
+>>> # stepvalue-> bydefault=1
---
+++ day19/Looping Statement/looping/rangefunctionEX.py
@@ -0,0 +1,5 @@
+#Range()---> startPoint----endPoint----stepvalue
+
+
+for i in range(10): #here startpoint->0 and stepvalue-1
+    print(i,end=" ")
