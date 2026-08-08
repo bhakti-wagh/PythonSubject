54b2827bb7947e01446ff8f13f32187d80ab2899
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-08 09:28:47 +0530

    create reversed function

---
+++ day19/Looping Statement/looping/Reversedfunction.py
@@ -0,0 +1,30 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#Reversed--->
+
+>>> #reversed(): ---> inbuilt function
+>>> 
+>>> #here reversed the data
+>>> 
+>>> #In reversed if we done operation directly it will show
+>>> #   object address
+>>> 
+>>> # In reversed to avoid object address data we have
+>>> #   Two ways:->
+>>> #       1.Typecasting
+>>> #       2.Looping
+>>> 
+>>> #Typecasting syntax:-->
+>>> #       list(reversed(iterable))
+>>> #       list(reversed(iterable))
+>>> #       list(reversed(iterable))
+>>> 
+>>> #       tuple(reversed(iterable))
+>>> #       dict(reversed(iterable))
+>>> #       set(reversed(iterable))
+>>> 
+>>> #Looping Syntax:-->
+>>> #   |--------------------------------------------|
+>>> 
+>>> #       for variable in reversed(iterable):
+>>> #       <->  statement
