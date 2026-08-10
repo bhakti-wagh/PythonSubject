1fdf662cd1e1762f7bddd0e69d4775e2ffc9e81c
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-10 09:36:43 +0530

    create programs

---
+++ day19/Looping Statement/looping/Programs.py
@@ -0,0 +1,24 @@
+#Programs
+
+
+#wap to print a-z character
+'''
+for i in range(97,123):
+    print(chr(i),end=" ")
+
+print()
+'''
+
+
+#Wap to print A-Z character
+'''
+for i in range(65,91):
+    print(chr(i),end=" ")
+'''
+
+#If you want to print a-z and A-Z character then use below logic
+'''
+for i in range(97,123):
+    print(chr(i),chr(i-32),end=" ")
+'''
+
