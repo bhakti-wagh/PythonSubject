fd48170bb9b2e626f453477dd5b1957604a7bc7b
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-10 09:03:23 +0530

    created Zip longest and Zip function

---
+++ day19/Looping Statement/looping/ZipFunction.py
@@ -0,0 +1,15 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Zip()--.
+>>> #Inbuilt function
+>>> #Output should be in Tuple form
+>>> #we have to pass same type of data
+>>> #it match position to position
+>>> 
+>>> #syntax:-> Zip(iterable1,iterable2...)
+>>> 
+>>> #If length is miss match data loss will be happen
+>>> 
+>>> 
+>>> #Iteriable length should be equal  .....
+.
--- day19/Looping Statement/looping/reversedEx.py
+++ day19/Looping Statement/looping/reversedEx.py
@@ -72,9 +72,69 @@ print()
 print()
 
 '''
-
+'''
 res=[]
 
 for i in d:
     res=[i]+res
 print(res)#[5, 4, 3, 2, 1]
+'''
+
+
+
+
+#26.Wap to check how many words are present
+#   in the given sentence
+
+'''
+a="hello world sentence"
+
+count=0
+
+for i in a.split():
+    count=count+1
+
+print(count)#3
+'''
+
+
+#30.wap to create a dictionary with element and its count pair
+'''
+l=["yellow","red","black","pink","orange","green","red","pink","yellow"]
+#output:-->
+#{'yellow': 2, 'red': 2, 'black': 1, 'pink': 2, 'orange': 1, 'green': 1}
+
+'''
+
+a='1,2,3,4,5,6,7,8,9'
+
+even=''
+odd=''
+
+for i in a.split(","):
+
+    
+    if int(i)%2==0:
+        even=even+i
+    else:
+        odd=odd+i
+
+print(even+odd)
+
+print()
+
+print()
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
