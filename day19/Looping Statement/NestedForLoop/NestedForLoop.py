fa11c0950fdd75470388327cfa1e1d73e4ed6289
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-11 09:53:14 +0530

    create nested for loop

---
+++ day19/Looping Statement/NestedForLoop/NestedForLoop.py
@@ -0,0 +1,15 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Nested For Loop---->
+>>> 
+>>> ##--->-->-->
+>>> 
+>>> # for outer in iterable:
+>>> # <-> Statement
+>>> #       for inner in interable:
+>>> #       <-> Statement
+>>> 
+>>> # Outer for loop execute only one time
+>>> # inner for loop execute completely
+>>> 
+>>> 
