b8a60a29d4639def8422c68edd62da0b1705ca18
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-11 09:38:58 +0530

    create pass statment

---
+++ day19/Looping Statement/statement(break,conti,pass)/pass.py
@@ -0,0 +1,9 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Pass Statment
+>>> 
+>>> #pass:-> if you want to avoid indentaiton ,Error, you can use pass
+>>> # -> in the place of {pass}->(...)
+>>> 
+>>> #Pass is keyword
+>>> #If you want to hold the block
