a2cfa91b3f7997b112b338a2e8cc970916da464e
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-11 09:23:06 +0530

    updated break statement

---
+++ day19/Looping Statement/statement(break,conti,pass)/examples.py
@@ -0,0 +1,20 @@
+
+d=[1,2,10,3,11]
+'''
+for i in d:
+
+    if i>=10:
+        break
+    print(i)
+
+'''
+
+s=[1,12,-3,90,-4,-5,900,-12]
+
+for i in s:
+
+    if i<0:
+        print(i)
+        break
+    
+    
