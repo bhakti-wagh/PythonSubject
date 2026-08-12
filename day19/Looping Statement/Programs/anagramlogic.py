80fcc5ad1a3d88342c26ff4c932257d8ac5e9068
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-12 09:23:15 +0530

    added armstrong example and logicj

---
+++ day19/Looping Statement/Programs/anagramlogic.py
@@ -0,0 +1,18 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Anagram:-
+>>> #:-> character should be same it can different meaning
+>>> #Ex:-> tea:eat, silent:listen, bored:robed, cat:act, keep:peek
+>>> 
+>>> #use sorted() method :-> it will arrange according to ascii value then it
+>>> #will easily compariason
+>>> #  ==
+>>> 
+>>> 
+>>> 
+>>> #Armstrong :-> sum of the number to the power
+>>> 
+>>> #EX:->    1 5 3:->   1^3  5^3  3^3 :-> 153
+>>> 
+>>> 
+>>> 
