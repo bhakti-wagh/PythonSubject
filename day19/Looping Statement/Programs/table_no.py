03509aed81490757214ce4e916eb048d82aaa7fe
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-12 09:36:16 +0530

    create table_no

---
+++ day19/Looping Statement/Programs/table_no.py
@@ -0,0 +1,17 @@
+
+#wap to print table of 2 to 10
+
+for i in range(1,11):
+
+    for j in range(2,11):
+
+        print(i*j,end=" ")
+    print()
+
+
+for i in range(1,11):
+
+    for j in range(1,11):
+        print(f'{i}*{j}--->{i*j}')
+    print()
+    
