83be3a60565be990fb38e16a49eaadad2e3b1830
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-12 08:57:34 +0530

    created anagram and program loop

---
+++ day19/Looping Statement/Programs/Program looo.py
@@ -0,0 +1,75 @@
+#1.WAP to return a dictionary with word & its len pair
+#from a string
+string = 'hello good morning how are youu'
+#exp o/p : {hello:5, guys:4, morning:7, how:3, are:3, you:4}
+'''
+
+"""
+2.WAP to count number of vowels present in given string
+s = 'GooD mOrnIng'
+"""
+
+"""
+3.WAP to get below o/p:
+s = 'Hi how are you'
+#exp o/p : 'iH woh era uoy'
+
+"""
+
+"""
+4.WAP to print all the digits in a below list
+# l = ['hello', '123', 'hai', 'python', '345']
+
+"""
+
+"""
+5.WAP to check whether string is ANAGRAM or not
+
+#anagrams : characters should be same it can different meaning
+#tea, eat
+#silent, listen
+#bored , robed
+#cat, act
+#keep, peek
+#lamp, palm
+
+"""
+
+"""
+6.Find the sum of even numbers from 1 to 20
+"""
+
+#7.Count numbers divisible by 3 from 1 to 50
+
+#8.Replace negative numbers with 0
+numbers = [10, -5, 20, -3, 40]
+
+#9.Print position of each character
+word = "PYTHON"
+"""
+1 P
+2 Y
+3 T
+4 H
+5 O
+6 N
+"""
+#10.Count even and odd numbers in a list.
+num= [10, 15, 22, 31, 40, 51]
+
+#11.wap to print repeated char and count the same
+s="helloworld"
+
+
+#12.Grouping flowers and animals separately
+items=["lotus-flower","lilly-flower","cat-animal","dog-animal","sunflower-flower"]
+
+
+#13.filter only character except digits
+s="Think456 and 123answers it789 guys "
+
+#14.replace whitespaces with newline char in the below string
+s="hello world welcome to python"
+
+#15.replace all vowels with *
+s="hello world welcome to python"
---
+++ day19/Looping Statement/Programs/anagramlogic.py
@@ -0,0 +1,8 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Anagram:-
+>>> #:-> character should be same it can different meaning
+>>> #Ex:-> tea:eat, silent:listen, bored:robed, cat:act, keep:peek
+>>> 
+>>> #use sorted() method :-> it will arrange according to ascii value then it
+>>> #will easily compariason
