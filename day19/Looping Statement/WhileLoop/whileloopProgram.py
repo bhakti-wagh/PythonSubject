21570060fe6dab64015b762f8f9ad2de377dfefa
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-14 09:22:23 +0530

    Update whileloopProgram.py

--- day19/Looping Statement/WhileLoop/whileloopProgram.py
+++ day19/Looping Statement/WhileLoop/whileloopProgram.py
@@ -1,5 +1,6 @@
 #Wap to find factor of given number
 
+'''
 num=eval(input("Enter number:"))
 
 i=1
@@ -8,7 +9,9 @@ out=[]
 
 while i<=num:
 
-    if num%2==0:
+    if num%i
+
+    ==0:
 
         out.append(i)
 
@@ -17,3 +20,52 @@ while i<=num:
 print(out)
 
     
+'''
+
+
+#perfect number
+'''
+num=eval(input("Enter number:"))
+
+i=1
+
+a=[]
+
+while i<num:
+
+    if num%i==0:
+        print(i)
+
+        a.append(i)
+
+    i+=1
+
+print(sum(a),"perfectnumber")
+
+
+   '''
+
+
+#Prime number
+'''
+num=eval(input("Enter number:"))
+
+i=1
+
+out=[]
+
+while i<=num:
+
+    if num%i==0:
+        out.append(i)
+    i+=1
+
+if len(out)==2:
+    print("prime number")
+
+else:
+    print("not a prime number")
+
+'''
+
+ 
