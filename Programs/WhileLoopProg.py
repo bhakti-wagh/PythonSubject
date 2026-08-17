b62dc80da6701ab0caaae12b244282c88e56f946
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-17 16:11:08 +0530

    Update WhileLoopProg.py

--- Programs/WhileLoopProg.py
+++ Programs/WhileLoopProg.py
@@ -11,15 +11,20 @@ while i<6:
 
 #wap to print multiplication table for n
 '''
-n=eval("Enter password:")
+n=eval(input("Enter number:"))
 
-while n=='123':
+i=1
 
-'''   
+while i<=10:
+
+    print(n,"*",i,"=",n*i)
+
+    i+=1
+'''
 
 #wap to run infinite loop until user enters the
 #correct password
-
+'''
 password='pythonE14'
 
 while True:
@@ -31,5 +36,26 @@ while True:
 
         break
     
+'''
+
+
+#wap to find sum of n natural number
+'''
+num=eval(input("Enter number:"))
+
+i=1
+
+total=0
+
+while i<=num:
+
+    total=total+i
+
+    i+=1
+
+print("Sum of natural numbe:",total)
+
+'''
+
 
 
