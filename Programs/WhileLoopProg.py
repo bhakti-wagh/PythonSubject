385c71adc549e439f9c314bd6871ba44c1fcd051
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-17 15:43:58 +0530

    Update WhileLoopProg.py

---
+++ Programs/WhileLoopProg.py
@@ -0,0 +1,21 @@
+# wap to print python for 5 times
+
+'''
+a="Python"
+i=1
+while i<6:
+    print(a)
+
+    i+=1
+'''
+
+#wap to print multiplication table for n
+
+n=eval("Enter password:")
+
+while n=='123':
+
+    
+
+#wap to run infinite loop until user enters the
+#correct password
