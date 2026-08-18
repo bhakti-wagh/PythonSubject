a5038c5bcc34c849817f89ed16c920e92b2462ba
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-18 14:03:58 +0530

    Create complexdatatype.py

---
+++ complex/complexdatatype.py
@@ -0,0 +1,19 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=98+10j
+>>> 
+>>> a.real
+98.0
+>>> 
+>>> a.imag
+10.0
+>>> 
+>>> 
+>>> a=None
+>>> 
+>>> a
+>>> 
+>>> 
+>>> 
+>>> # to find real and imag part we use (varname.real)(varname.imag)
+>>> 
