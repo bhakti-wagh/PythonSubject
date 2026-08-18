3afe0ec695e5a395e3bfb56c558de0d8c859993e
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-18 14:19:33 +0530

    Create string.py

---
+++ practiceCollectionDT/string.py
@@ -0,0 +1,6 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #String:->  It is sequence of character enclosed with quotes
+>>> 
+>>> # varname="value"
+>>> 
