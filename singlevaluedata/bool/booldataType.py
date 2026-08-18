b89b45e4b3cd3579eb6cebd7ed74e8e2115c5e3b
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-18 14:11:24 +0530

    Update booldataType.py

--- singlevaluedata/bool/booldataType.py
+++ singlevaluedata/bool/booldataType.py
@@ -2,3 +2,12 @@ Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (
 Enter "help" below or click "Help" above for more information.
 >>> #Boolean Data type:
 >>> 
+>>> 
+>>> #It is a individual data type which consist of only two value in its
+>>> #True / False
+>>> 
+>>> #True and False are special keywords
+>>> 
+>>> #"True":-> it is non -default value
+>>> #"False":-> it is default value
+>>> 
