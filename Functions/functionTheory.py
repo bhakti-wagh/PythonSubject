44342525c2f69fdafd7b598a726cbcc537c58d33
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-25 15:10:12 +0530

    Create functionTheory.py

---
+++ Functions/functionTheory.py
@@ -0,0 +1,9 @@
+#Function:->
+
+#why we want to use Function:->
+#if you want to avoid code repetation , same code again i want to reuse
+#code in systamatic order
+
+
+
+#Defination:->   Set of code / Block of code It will execute When we calling the function
