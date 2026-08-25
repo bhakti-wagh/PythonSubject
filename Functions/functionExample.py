bb08ec82dce5404c2f8cd366335b7b73ae1cc841
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-25 16:21:00 +0530

    Update functionExample.py

--- Functions/functionExample.py
+++ Functions/functionExample.py
@@ -11,3 +11,17 @@ def odd(a):
         print(f"even number")
 
 odd(11)
+
+
+
+
+def even_odd():
+    num=int(input("Enter the number:"))
+
+    if num%2==0:
+        print(f"{num} is even number")
+
+    else:
+        print(f"{num} is odd number")
+
+even_odd()
