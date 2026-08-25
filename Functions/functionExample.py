5843ff743e3ecbb08d521b54a0d8f6c015844788
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-25 16:28:49 +0530

    Update functionExample.py

--- Functions/functionExample.py
+++ Functions/functionExample.py
@@ -3,7 +3,7 @@
 
 #wap to print odd number:
 
-
+'''
 def odd(a):
     if a%2==1:
         print(f"Odd number {a}")
@@ -11,9 +11,9 @@ def odd(a):
         print(f"even number")
 
 odd(11)
+'''
 
-
-
+'''
 
 def even_odd():
     num=int(input("Enter the number:"))
@@ -25,3 +25,49 @@ def even_odd():
         print(f"{num} is odd number")
 
 even_odd()
+'''
+
+
+#wap to check the given number is palindrome
+'''
+s="level"
+
+if s==s[::-1]:
+    print("palilndrome")
+else:
+    print("not a palilndrome")
+'''
+'''
+def palindrome():
+     x=eval(input("Enter the string:"))
+
+     if x==x[::-1]:
+         print("its pal")
+     else:
+         print("not a pal")
+palindrome()
+
+'''
+'''
+
+s=[1,2,3,4,5,6,10]
+
+
+for i in s:
+
+    if i%2==0:
+        print(i)
+'''
+
+def even_Data(s):
+
+    for i in s:
+        if i%2==0:
+            print(i,end=" ")
+
+even_Data([1,2,3,4,5,6,10])
+
+
+
+
+        
