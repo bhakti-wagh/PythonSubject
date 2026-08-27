764a7364993d1e239cfbd4cd284b5bcd6b0d11cc
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-27 15:53:18 +0530

    Create scopetheory.py

---
+++ scope/scopetheory.py
@@ -0,0 +1,19 @@
+                               #Scope
+
+# The place of the variable
+
+# Types of Scope:
+
+# 1. Local variable
+# 2. Global Variable
+# 3. NonLocal variable
+
+
+
+# > Local Variable :
+
+# 1. Any variable present inside the function then we can it as a local variable
+# 2. Local variable we can't access outside directly if we access it will show --NameError--
+
+# 3. How to Access local variable outside  By the help of return  Keyword
+
