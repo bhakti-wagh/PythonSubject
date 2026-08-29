700d472f1a9b38cdbd9a58ff08b258f3e0db7e18
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-29 11:56:44 +0530

    Update functionvsgenerator.py

--- Functions/FunctionTask/functionvsgenerator.py
+++ Functions/FunctionTask/functionvsgenerator.py
@@ -1,4 +1,5 @@
-
+'''
+#using return keyword
 def check(x,y):
     a=x+y
     b=x-y
@@ -13,6 +14,8 @@ print(d)  #o/p:30
 
 print()
 
+
+#Using generator with yield keyword
 def check(x,y):
     a=x+y
     b=x-y
@@ -23,8 +26,12 @@ def check(x,y):
 
 d=check(10,20)
 print(d)  #o/p:<generator object check at 0x0000028A9E9E0BA0>
+#If we use print() : it will show object address
 
 
+
+
+#1st typecasting
 def check(x,y):
     a=x+y
     b=x-y
@@ -36,6 +43,66 @@ def check(x,y):
 d=check(10,20)
 #typecasting use
 print(list(d)) #o/p: [30, -10, 200]
+
+print()
+
+
+#2.using looping
+def check(x,y):
+    a=x+y
+    b=x-y
+    c=x*y
+    yield a
+    yield b
+    yield c
+
+d=check(10,20)
+#using loop
+
+for i in d:
+    print(i)  # 30 ,-10,200
+
+print()
+
+
+
+
+#Using next() :-> it will call one by one output
+def check(x,y):
+    a=x+y
+    b=x-y
+    c=x*y
+    yield a
+    yield b
+    yield c
+
+d=check(10,20)
+print(next(d))#30
+print(next(d))#-10
+print(next(d))#200
+print(next(d))#StopIteration
+
+
+
+'''
+
+
+'''
+
+#If i want at time output using geneartor with yield keyword
+#o/p : in tuple form
+def check(x,y):
+    yield x+y,x-y,x*y
+
+d=check(10,20)
+print(next(d)) #(30, -10, 200)
+
+'''
+
+
+
+
+
 
 
 
