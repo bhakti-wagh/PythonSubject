583a3f2d66782c2794f2deec94db2588d5c13a20
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-29 11:42:10 +0530

    Update functionvsgenerator.py

--- Functions/FunctionTask/functionvsgenerator.py
+++ Functions/FunctionTask/functionvsgenerator.py
@@ -11,4 +11,33 @@ d=check(10,20)
 print(d)  #o/p:30
 
 
+print()
+
+def check(x,y):
+    a=x+y
+    b=x-y
+    c=x*y
+    yield a
+    yield b
+    yield c
+
+d=check(10,20)
+print(d)  #o/p:<generator object check at 0x0000028A9E9E0BA0>
+
+
+def check(x,y):
+    a=x+y
+    b=x-y
+    c=x*y
+    yield a
+    yield b
+    yield c
+
+d=check(10,20)
+#typecasting use
+print(list(d)) #o/p: [30, -10, 200]
+
+
+
+
 
