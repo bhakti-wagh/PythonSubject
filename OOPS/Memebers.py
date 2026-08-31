5d867e3f8c0c4cd1786dd79c30bdc09070957db7
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-31 15:05:51 +0530

    Update Memebers.py

--- OOPS/Memebers.py
+++ OOPS/Memebers.py
@@ -19,6 +19,65 @@ print(total_sub)
 
 # we can't access class data outside it will show "NameError"
 '''
-#How to Access class Data outside
+'''
+How to Access class Data/variable outside
+-------------------------------------------
+
+Here we have Two ways:
+1.By using ClassName
+2.By using object
+
+'''
+
+#1. By using ClassName
+
+print(f"Student Name is:{Student.name}")
+print(f"Student Age:{Student.age}")
+print(f"Student Total Subject:{Student.total_sub}")
+
+'''
+o/p: Student Name is:bhakit
+Student Age:23
+Student Total Subject:5
+'''
+
+#2.By using object
+print(f"Student Name is:{s.name}")
+print(f"Student Age:{s.age}")
+print(f"Student Total Subject:{s.total_sub}")
+
+'''
+o/p: Student Name is:bhakit
+Student Age:23
+Student Total Subject:5
+
+'''
+
+
+
+#In class :Inside the class whatever the data stored internally in form of : dictionary{key:value}
+
+
+#If you want to check how the data is stored in internally:
+
+#print(ClassName.__dict__)
+
+
+print(Student.__dict__)
+
+
+#docstring:-> description of the clas
+#how to print doc or title of class
+#:-> print(classname.__doc__)
+
+#If you want to complete information of the class
+#:-> help(className)
+help(Student)
+
+
+
+
+
+
 
 
