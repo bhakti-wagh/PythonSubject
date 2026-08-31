ab7e0563d02d8fd486a84d7f0240bd67f5944777
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-31 22:31:04 +0530

    Create oopsTheory.py

---
+++ OOPS/oopsTheory.py
@@ -0,0 +1,36 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#OOPS:
+#Object Oriented programming Structure or System:
+
+
+# Def: To Represent the real world object to software world by using
+#       class and object
+
+# Why we use OOPS:
+>>> # - easily stored data
+>>> # - easily accessed data
+>>> # - Maintained data properly
+>>> # - security purpose also we use
+>>> # - reusability
+>>> 
+>>> 
+>>> # What is class: class is a blueprint,design ,plan, module of the object
+>>> # What is object: it is a physical existence of class
+>>> 
+>>> #By using 1 class we can create n.no.of object
+>>> 
+>>> 
+>>> #How to create a empty class?:
+>>> 
+>>> #  class ClassName:  ---name of the class
+>>> #       pass/...
+>>> 
+>>> #How to create a object ?:
+>>> 
+>>> # newVariable_name=ClassName()
+>>> 
+>>> #   newVariable :-> reference variable
+>>> 
+>>> #Reference Varaible:-> Any variable referencing to the object tha called as reference var.
+>>> 
