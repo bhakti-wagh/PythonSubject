a636466c4da4460ea217c1d2b267a303773f6c29
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-09-02 12:35:23 +0530

    Create Methods.py

---
+++ OOPS/Methods.py
@@ -0,0 +1,51 @@
+
+#Methods:
+
+# def: A function inside the class we can call it as Methods
+
+# 3 types of methods:
+# 1. instance Method
+# 2. Class Method
+# 3. Static Method
+
+
+#What is the meaning of Instance Method?
+#-> A method it will accept only first parameter of the object address
+# -> then we call it as instance Method
+#-> by default it take [Self]  special parameter
+
+
+
+#Syntax:->    class ClassName:
+#
+#                   def Method_name(self):
+#                           stmt
+#           object=ClassName()
+
+
+# : self is always pointing to a object
+# : it is a special parameter
+
+#> if we want to access instance method it is mandatory to create a object
+
+# To fetch  instance Method outside we have [2] ways:
+#  1. by using className
+#  2. by using object
+
+
+
+#1. by using className:  expecility we have to use object
+#       syntax :    ClassName.method(object)
+
+
+#2. by using object:  internally it take a object
+#       syntax:    object.method() :-> internally :-> object.method(object)
+
+
+
+
+
+
+
+
+
