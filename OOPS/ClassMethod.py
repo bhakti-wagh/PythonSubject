a1cdee73caf79c14db46e84f343157e6cdb94edc
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-09-05 13:01:17 +0530

    Create ClassMethod.py

---
+++ OOPS/ClassMethod.py
@@ -0,0 +1,95 @@
+
+#Class Method:->
+
+#Class Method only working for Class Data
+
+#In class method object creation not required/ not mandadatory
+
+#In class method by default it will take it as a 'cls'parameter
+
+#cls:-> specail parameter , it will pointing to main class
+
+#DEF:-> Any method decorated with  '@classmethod', then we call it as
+#       class Method
+
+#* :-> class method only working for class data
+
+#Syntax:->
+#           class classname:
+
+#                   @classmethod
+#                   def method_name(cls):
+#                       stmt
+#           object=classname():-> not required
+
+
+#   Access Through object
+#       object.method_name()
+
+#  Access Through classname
+#       classname.method_name()
+
+
+# while you miss @classmethod it will act as an 'instance Method'
+
+
+'''
+class wish:
+
+    @classmethod
+    def greet(cls):
+
+        print(cls)
+
+
+w=wish()
+
+print(w)#<__main__.wish object at 0x0000017D7A97AE40>
+
+w.greet()#<class '__main__.wish'>
+
+
+wish.greet()#<class '__main__.wish'>
+'''
+
+
+
+
+#Access class variable into the class method:
+#   2 ways:-> 1. using cls
+#             2. using classname
+
+#Both will be work
+
+'''
+
+#EX:
+
+#Using cls
+class Car:
+    name="BMW"
+
+    @classmethod
+    def data(cls):
+        print(cls.name)
+
+
+Car.data()#BMW
+
+
+
+#using classname
+class Flower:
+    name="Rose"
+
+    @classmethod
+    def data(cls):
+        print(Flower.name)
+
+Flower.data()#Rose
+'''
+
+
+
+
+#Modification in class method
