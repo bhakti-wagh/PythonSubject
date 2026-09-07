4b562a9184508aae358de3cd042581e67a2db83f
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-09-07 16:07:38 +0530

    Update Constructor.py

--- OOPS/Constructor.py
+++ OOPS/Constructor.py
@@ -24,5 +24,79 @@ b=Bhakti() #Constructor Overloading
 
 '''
 
+'''
+class Student:
+
+    def __init__(self):
+
+        print("Student class")
+
+    def show(self):
+        print("Show class")
+
+s=Student()
+
+s.show()
+'''
+
+
+'''
+
+#calling instance method into the constructor
+
+class Student:
+
+    def __init__(self):
+
+        print("Student class")
+
+        self.show()
+
+    def show(self):
+        print("Show class")
+
+s=Student()
+
+'''
+
+'''
+class Car:
+
+    def __init__(self):
+
+        #instance variable
+
+        self.name="BMW"
+        self.color="Red"
+        self.cost="1cr"
+
+        print(f"my car name :{self.name}\n my car color: {self.color}\n car cost is:{self.cost}")
+
+
+x=Car()
+
+'''
+
+
+#Ex6: constructor + instance method without parameter
+
+
+class Car:
+
+    def __init__(self):
+
+        #instance variable
+
+        self.name="BMW"
+        self.color="Red"
+        self.cost="1cr"
+
+    def show(self):
+        print(f"my car name :{self.name}\n my car color: {self.color}\n car cost is:{self.cost}")
+
+
+x=Car()
+
+        
 
 
